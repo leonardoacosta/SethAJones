51f05c5b4919f405a095875d1912845eef3a47a3
Leonardo Acosta <Leo@LeonardoAcosta.dev>
2023-09-11 13:57:53 -0500

    Pause for transfer

--- app/page.tsx
+++ app/page.tsx
@@ -19,44 +19,39 @@ export default function Home() {
 
 	return (
 		<>
-			<div className='flex flex-col  items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black relative'>
-				<Image
-					src='/imgs/16.jpg'
-					alt='Seth Jones'
-					height={2000}
-					width={2000}
-					className='animate-title absolute left-0 w-1/2 lg:max-w-xl filter mix-blend-difference opacity-60'
-				/>
-				<Image
-					src='/imgs/32.jpg'
-					alt='Seth Jones'
-					height={2000}
-					width={2000}
-					className='animate-title absolute right-0 w-1/2 lg:max-w-xl filter mix-blend-difference opacity-60'
-				/>
-
-				<nav className='my-16 animate-fade-in mix-blend-difference'>
-					<ul className='flex items-center justify-center gap-4'>
-						{navigation.map((item) => (
-							<Link key={item.href} href={item.href} className='text-sm duration-500 text-zinc-300 hover:text-zinc-300'>
-								{item.name}
-							</Link>
-						))}
-					</ul>
-				</nav>
+			<div className='flex flex-col items-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black relative'>
+				<div>
+					<nav className='my-8 animate-fade-in mix-blend-difference'>
+						<ul className='flex items-center justify-center gap-4'>
+							{navigation.map((item) => (
+								<Link key={item.href} href={item.href} className='text-sm duration-500 text-zinc-300 hover:text-zinc-300'>
+									{item.name}
+								</Link>
+							))}
+						</ul>
+					</nav>
+				</div>
 				<div className='hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0' />
-				<Particles className='absolute inset-0 -z-10 animate-fade-in' quantity={100} />
+
 				<h1 className='mix-blend-difference z-10 text-3xl font-signature text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text '>
 					Seth A. Jones
 				</h1>
 
 				<div className='hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0' />
 				<div className='my-16 text-center animate-fade-in mix-blend-difference'>
-					<h2 className='text-sm text-zinc-300'>Seth Jones a Actor and Voice Actor on the rise.</h2>
+					<h2 className='text-sm text-zinc-300'>Seth Jones - fall in love with the process</h2>
 					<h2 className='w-full pt-8' onClick={scrollToEnd}>
 						<ChevronDown className='text-white animate-bounce mx-auto' />
 					</h2>
 				</div>
+				<Particles className='absolute inset-0 -z-10 animate-fade-in' quantity={100} />
+				<Image
+					src='/imgs/16.jpg'
+					alt='Seth Jones'
+					height={2000}
+					width={2000}
+					className='animate-title  object-center mx-auto w-1/2 lg:max-w-xl filter mix-blend-difference opacity-60'
+				/>
 			</div>
 			<div className='px-6 my-24 md:max-w-4xl mx-auto text-zinc-300'>
 				<h1 className='text-xl my-2 text-zinc-300 text-center'>About</h1>
